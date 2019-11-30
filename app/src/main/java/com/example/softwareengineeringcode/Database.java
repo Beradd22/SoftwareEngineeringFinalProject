61774fdde5b1c2e97a734c1c9abe5583aa7db5e2
Khari Berger <khariandberger@yahoo.com>
2019-11-29 21:31:17 -0500

    Updated Funcs; getFuncs not fully done -> creating a test app that will test output of the cursor's; The AccessDB Class will be made once the DB Class is finished

--- app/src/main/java/com/example/softwareengineeringcode/Database.java
+++ app/src/main/java/com/example/softwareengineeringcode/Database.java
@@ -7,94 +7,88 @@ import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
-    class Database {
-        SQLiteDatabase db;
+class Database {
+    SQLiteDatabase db;
 
 
-        public String getReports() {
-            Report report = new Report();
-            String DBFileLocation = "C:\\Users\\khari\\AndroidStudioProjects\\SQLiteDBPractice\\Assets";
-            String DBFileName = "IncidentReportApp.db";
-            String query = "SELECT * From Reports";
-            List<Report> reportList = new ArrayList<>();
+    public List<Report> getReports() {
+        Report report = new Report();
+        String DBFileLocation = "SoftwareEngineeringFinalProject\\app\\src\\main\\assets\\databases\\";
+        String DBFileName = "IncidentReportApp.db";
+        String query = "SELECT * From Reports";
+        List<Report> reportList = new ArrayList<>();
 
-            try {
-                //Open Database
-                db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
-                Cursor cursor = db.rawQuery(query, null);
-                if (cursor.moveToFirst()) {
-                    while (cursor.moveToNext()) {
-                        for (int i = 0; i < cursor.getCount(); i++) {
-                            String row = cursor.getString(i);
-                            return row;
-                            //PUT DATA INTO report OBJ then add OBJ to reportList
+        try {
+            //Open Database
+            db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
+            Cursor cursor = db.rawQuery(query, null);
+            if (cursor.moveToFirst()) {
+                while (cursor.moveToNext()) {
+                    for (int i = 0; i < cursor.getCount(); i++) {
+                        String row = cursor.getString(i);
+                        //PUT DATA INTO report OBJ then add OBJ to reportList
 
-                        }
+                        reportList.add(report);
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            //return reportList;
-            return "yo";
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        //return reportList;
+        return reportList;
+    }
 
 
-        public String getReport(Report r) {
-            Report report = new Report();
-            String DBFileLocation = "C:\\Users\\khari\\AndroidStudioProjects\\SQLiteDBPractice\\Assets";
-            String DBFileName = "IncidentReportApp.db";
-            String query = "SELECT 1 From Reports where Reports.Title=" + r.title + " AND Reports.DateTime=" + r.dateTime + ")";
+    public Report getReport(Report r) {
+        Report report = new Report();
+        String DBFileLocation = "SoftwareEngineeringFinalProject\\app\\src\\main\\assets\\databases\\";
+        String DBFileName = "IncidentReportApp.db";
+        String query = "SELECT 1 From Reports where Reports.Title=" + r.title + " AND Reports.DateTime=" + r.dateTime + ")";
 
-            try {
-                //Open Database
-                db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
-                Cursor cursor = db.rawQuery(query, null);
-
-                String row = cursor.getString(0);
-                return row;
-                //PUT DATA INTO report OBJ then return the OBJ
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //return reportList;
-            return "yo";
-        }
-
-
-        private String submitRecord(Report r, Blob b) {
-            String DBFileLocation = "C:\\Users\\khari\\AndroidStudioProjects\\SQLiteDBPractice\\Assets";
-            String DBFileName = "IncidentReportApp.db";
-
-            //Add to Reports table in DB
-            String SQL = "INSERT INTO Reports(Title,WeatherType,Location,DateTime, Details) VALUES (" + r.title + "," + r.wType + "," + r.location + "," + r.dateTime + r.details + ")";
-
+        try {
             //Open Database
-            try {
-                db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
-                db.execSQL(SQL);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            String query = "SELECT 1 From Reports ORDER BY ReportID DESC";
+            db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
             Cursor cursor = db.rawQuery(query, null);
 
-            if (cursor.moveToFirst()) {
-                //Grab NEWEST Report ID and store it
-                String row = cursor.getString(0);
-                //return row;
-            }
+            String row = cursor.getString(0);
+            //PUT DATA INTO report OBJ then return the OBJ
+
+            return report;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //return reportList;
+        return report; //will be NULL
+    }
 
 
-            for (int i = 0; i < r.pictures.size() - 1; i++) {
-                //Add to Pictures table in DB
-                SQL = "INSERT INTO Pictures(ReportID, Binary) VALUES (" + cursor.getString(0) + b.toString() + ")";
-            }
+    private void submitRecord(Report r, Blob b) {
+        String DBFileLocation = "SoftwareEngineeringFinalProject\\app\\src\\main\\assets\\databases\\";
+        String DBFileName = "IncidentReportApp.db";
 
-            return "bad";
+        //Add to Reports table in DB
+        String SQL = "INSERT INTO Reports(Title,WeatherType,Location,DateTime,Details) VALUES (" + r.title + "," + r.wType + "," + r.location + "," + r.dateTime + r.details + ")";
+
+        //Open Database
+        try {
+            db = SQLiteDatabase.openOrCreateDatabase(DBFileLocation + DBFileName, null);
+            db.execSQL(SQL);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        String query = "SELECT MAX(ReportID) From Reports";
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            for (int i = 0; i < r.pictures.size() - 1; i++) {
+                //Add to Pictures table in DB
+                SQL = "INSERT INTO Pictures(ReportID, Binary) VALUES (" + cursor.toString() + b + ")";
+                db.execSQL(SQL);
+            }
+        }
     }
+
+}
